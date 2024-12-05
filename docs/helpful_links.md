6e74661d7dbd5026111f4dfcf766b35429e29da1
Bart Smits <bart@seza.si>
2024-12-05 17:05:20 +0100

    Improved categories by seperating them, improved dynamic adding of lines using htmx.

---
+++ docs/helpful_links.md
@@ -0,0 +1,10 @@
+
+[Useful code](https://github.com/trandi/esp32-thermal_printer)
+
+[Inspiration](https://www.youtube.com/watch?v=MZT0gV6-M9w)
+
+![Thermal printer](https://i5.walmartimages.com/seo/Thermal-Receipt-Printer-80mm-Desktop-Direct-Printing-USB-Connection-300mm-s-High-Speed-Auto-Cutter-Support-ESC-POS-Shipping-Business-Restaurant-Kitch_b1fb0bc1-5692-4c62-bcd1-e0f806bb529e.28fb62aa49f0b1d1524440c16d8c59c0.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF)
+
+[Drivers for TSP100](https://github.com/sezabart/TSP100-Ubuntu-Debian-driver)
+
+[Minimal LibreOffice install](https://askubuntu.com/questions/519082/how-to-install-libre-office-without-gui)
--- server.py
+++ server.py
@@ -13,7 +13,7 @@ import requests
 
 db = database('sqlite3.db')
 
-bills, bill_materials, materials = db.t.bills, db.t.bill_materials, db.t.materials
+bills, categories, materials = db.t.bills, db.t.categories, db.t.materials
 if bills not in db.t:
     bills.create(
         id=int,
@@ -25,21 +25,32 @@ if bills not in db.t:
         pk='id'
     )
 
+if categories not in db.t:
+    categories.create(
+        id=int,
+        name=str,
+        unit=str,
+        pk='id'
+    )
+    categories.insert(name='3D Filament', unit='g')
+    categories.insert(name='Laser Cutting', unit='mm²')
+
 if materials not in db.t:
     materials.create(
         id=int,
-        category=str,
+        category=int,
         nice_name=str,
         system_name=str,
-        unit=str,
         cost_per_unit=float,
         pk='id'
     )
-    materials.insert(category='3D Filament', nice_name='PLA', system_name='pla', unit='g', cost_per_unit=0.05)
-    materials.insert(category='3D Filament', nice_name='PETG', system_name='petg', unit='g', cost_per_unit=0.05)
+    materials.insert(category=1, nice_name='PLA', system_name='pla', cost_per_unit=0.05)
+    materials.insert(category=1, nice_name='PETG', system_name='petg', cost_per_unit=0.05)
+    materials.insert(category=2, nice_name='Acrylic', system_name='acrylic', cost_per_unit=0.05)
+    materials.insert(category=2, nice_name='Plywood', system_name='plywood', cost_per_unit=0.05)
 
 # Create a dataclass for the bills table entries
-Bill, BillMaterial, Material = bills.dataclass(), bill_materials.dataclass(), materials.dataclass()
+Bill, Category, Material = bills.dataclass(), categories.dataclass(), materials.dataclass()
 
 
 # This will be our 404 handler, which will return a simple error page.
@@ -62,28 +73,52 @@ rt = app.route
 def get():
     frm = Form(
         H3('Create a material bill:'),
-        Input(name='user', placeholder='First & Last Name', required=True, autocomplete="off", style='max-width: 200px;'),
+        Group(H5('User:'), Input(name='user', placeholder='First & Last Name', required=True, autocomplete="off", style='max-width: 200px;')),
+        Group(H5('Category:'), 
+            Select(
+                Option('Select a category', value='', disabled=True, selected=True, ),
+                *[Option(c.name, value=c.id) for c in categories()],
+                name='category',
+                required=True,
+                autocomplete="off",
+                style='max-width: 200px;',
+                hx_get='/material_select_row', hx_trigger='change, click from:next button', hx_target='#material_select_rows', hx_swap='beforeend', hx_include='category'
+            ),
+            Button('+', type='button', style='max-width: 50px;')
+            
+        ),
         Table(
             Tr(
                 Th('Material'),
-                Th('Quantity')
+                Th('Quantity'),
+                Th('Unit'),
             ),
-            material_select_row(),
+            id='material_select_rows'
         ),
         Button('Save & Print'),
         P('Printing...', id="printing", cls="htmx-indicator"),
-        hx_post='/print', hx_swap='afterend', hx_indicator="#printing"
+        hx_post='/print', hx_swap='afterend', hx_indicator="#printing",
+        name='material_bill'
     )
 
     return Titled("🛠️ FabLab's Bill 💶", frm)
 
 @rt("/material_select_row")
-def material_select_row(search: str=None, order_by: str='category'):
+def material_select_row(category: str=None):
+    if category:
+        cat = categories[category]
+        search_arg = {'where': f"category='{cat.id}'"}
+        category = cat.name
+        unit = f' [{cat.unit}]'
+    else:
+        search_arg = {'order_by': 'category'}
+        unit = ''
+    
     return Tr(
                 Td(
                     Select(
-                        Option('Select a material', value='', disabled=True, selected=True),
-                        *[Option(f"{m.nice_name} [{m.unit}]", value=m.id) for m in materials(search, order_by=order_by)],
+                        Option(f'Select {category} material', value='', disabled=True, selected=True),
+                        *[Option(f"{m.nice_name}", value=m.id) for m in materials(**search_arg)],
                     name='material',
                     ),
                 ),
@@ -91,8 +126,10 @@ def material_select_row(search: str=None, order_by: str='category'):
                     Input(type='number', name='quantity', autocomplete="off",),
                     style='max-width: 50px;'
                 ),
-            hx_swap='afterend', hx_trigger='change once', hx_get='/material_select_row',
+                Td(unit),
             )
+
+@rt("/save") # Make the Bill entry including calculated costs.
 
 @rt("/print")
 def post(user: str, material: list[int], quantity: list[int]):
